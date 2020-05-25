6a5e0b674f43f3e3d734580f73eb5f54d8563f71
Llewellyn Fernandes <llewellynkbc@gmail.com>
2020-05-25 19:55:53 +0200

    Added dune to Kalisalz buoy

--- machine/append-hw-common/kalisalz-buoy.mk
+++ machine/append-hw-common/kalisalz-buoy.mk
@@ -1,10 +1,11 @@
 include machine/append-sw-common/qemu-target.mk
 
+LAYERS += $(SOURCES_DIR)/meta-evo-private
+
+LOCAL_CONF_OPT += 'IMAGE_INSTALL_append = " dune-kalisalz-comm-${EVOSN} dune-kalisalz-comm-${EVOSN}-etc dune-kalisalz-comm-${EVOSN}-default" '
+
 LOCAL_CONF_OPT += 'hostname_pn-base-files = "kalisalz-buoy-$${EVOSN}"'
 
 LOCAL_CONF_OPT += 'BRIDGE_ADDRESS="192.168.$${EVOSN}.2"'
 
 LOCAL_CONF_OPT += 'BRIDGE_GATEWAY="192.168.$${EVOSN}.1"'
-
-
-
