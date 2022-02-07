74bd1f858ae108fd99464ab465871187dc3e9f2e
Victor Voronin <viktor.voronin@evologics.de>
2022-02-07 18:03:42 +0100

    machine/append-sw-common: chage the way pkgs installed in swupdate.mk

--- machine/append-sw-common/swupdate.mk
+++ machine/append-sw-common/swupdate.mk
@@ -1,5 +1,5 @@
 LAYERS += git@gitlab.evologics.de:bsp/meta-evo-private.git
 
-#Install SWUPDATE
-LOCAL_CONF_OPT    += 'IMAGE_INSTALL_append = " swupdate swupdate-www"'
+# Install SWUPDATE
+LOCAL_CONF_OPT    += 'CORE_IMAGE_EXTRA_INSTALL += "swupdate swupdate-www"'
 LOCAL_CONF_OPT 	  += 'PREFERRED_VERSION_swupdate = "2021.04"'
