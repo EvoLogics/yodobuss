292644e2232cbb6311da61c21770b3a503a47ffa
Victor Voronin <viktor.voronin@evologics.de>
2022-05-05 15:15:58 +0200

    machine/append-sw-common: use evo webapp for swupdate
    and move nodejs-native packageconfig into a separate file
    this affects sinaps (client) and swupdate (webapp)
    and potentially any recipe that requires nodejs-native

--- machine/append-sw-common/sinaps.mk
+++ machine/append-sw-common/sinaps.mk
@@ -1,7 +1,8 @@
+include machine/append-sw-common/nodejs-native.mk
+
 # Erlang OTP21
 LOCAL_CONF_OPT += 'PREFERRED_VERSION_erlang = "21.1%"'
 LOCAL_CONF_OPT += 'PREFERRED_VERSION_erlang-native = "21.1%"'
-LOCAL_CONF_OPT += 'PACKAGECONFIG_pn-nodejs-native = "ares brotli zlib"'
 
 # SiNAPS lives in meta-evo-private
 LAYERS += git@gitlab.evologics.de:bsp/meta-evo-private.git
--- machine/append-sw-common/swupdate.mk
+++ machine/append-sw-common/swupdate.mk
@@ -1,5 +1,7 @@
+include machine/append-sw-common/nodejs-native.mk
+
 LAYERS += git@gitlab.evologics.de:bsp/meta-evo-private.git
 
 # Install SWUPDATE
-LOCAL_CONF_OPT    += 'CORE_IMAGE_EXTRA_INSTALL += "swupdate swupdate-www swupdate-hwrevision"'
+LOCAL_CONF_OPT    += 'CORE_IMAGE_EXTRA_INSTALL += "swupdate swupdate-webapp-evo swupdate-hwrevision"'
 LOCAL_CONF_OPT 	  += 'PREFERRED_VERSION_swupdate = "2021.04"'
