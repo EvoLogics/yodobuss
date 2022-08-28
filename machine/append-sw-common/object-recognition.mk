de1c79b8fd63cd70b053188c4e709c558d3da825
llewellyn-evo <llewellyn.fernandes@evologics.de>
2022-08-28 13:37:56 +0200

    Add Basic config for object detection

--- machine/append-sw-common/object-recognition.mk
+++ machine/append-sw-common/object-recognition.mk
@@ -1,12 +1,6 @@
 LAYERS += git@gitlab.evologics.de:bsp/meta-evo-private.git
 LAYERS += https://git.yoctoproject.org/meta-java
 #Install docker and object recognition software
-LOCAL_CONF_OPT    += 'IMAGE_INSTALL_append = "	packagegroup-tegra-docker object-recognition"'
-#Install Java
-# Possible provider: cacao-initial-native and jamvm-initial-native
-#LOCAL_CONF_OPT    += 'PREFERRED_PROVIDER_virtual/java-initial-native = "cacao-initial-native"'
-# # Possible provider: cacao-native and jamvm-native
-#LOCAL_CONF_OPT    += 'PREFERRED_PROVIDER_virtual/java-native = "cacao-native"'
-#LOCAL_CONF_OPT    += 'PREFERRED_PROVIDER_java2-runtime = " openjdk-8-jre"'
-#LOCAL_CONF_OPT    += 'IMAGE_INSTALL_append = " openjdk-8"'
+LOCAL_CONF_OPT    += 'IMAGE_INSTALL_append = "	object-detector image-generator"'
 
+LOCAL_CONF_OPT 		+= 'hostname_pn-base-files = "sonobot-$${EVOSN}-tegra"'
