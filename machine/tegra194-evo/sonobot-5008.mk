7a707e0a814caa6736649c95e71b9311ea720381
llewellyn-evo <llewellyn.fernandes@evologics.de>
2022-09-09 17:53:27 +0200

    Clean up configuration for sonobot-5008

--- machine/tegra194-evo/sonobot-5008.mk
+++ machine/tegra194-evo/sonobot-5008.mk
@@ -1,8 +1,6 @@
 include machine/tegra194-evo/default.mk
 include machine/append-sw-common/object-recognition.mk
 include machine/append-sw-common/tegra-signing.mk
-
-#LOCAL_CONF_OPT   += 'IMAGE_INSTALL_append = " vsftpd python3 python3-flask python3-click python3-requests python3-numpy python3-pyyaml python3-jinja2 python3-itsdangerous python3-werkzeug"'
 # Enable all 6 cores by setting nvpmodule to mode 2
 LOCAL_CONF_OPT += 'NVPMODEL_CONFIG_DEFAULT = "2"'
 # Sonobot number
