5cdfbec06eca934d539f1a9fa3887c2f80ffcb69
llewellyn-evo <llewellyn.fernandes@evologics.de>
2022-09-20 11:06:48 +0200

    Add configuration for sonobot-50{69-77}

---
+++ machine/tegra194-evo/sonobot-5076.mk
@@ -0,0 +1,10 @@
+include machine/tegra194-evo/default.mk
+include machine/append-sw-common/object-recognition.mk
+
+LOCAL_CONF_OPT   += 'IMAGE_INSTALL_append = " db-agent"'
+# Enable all 6 cores by setting nvpmodule to mode 2
+LOCAL_CONF_OPT += 'NVPMODEL_CONFIG_DEFAULT = "2"'
+# Sonobot number
+LOCAL_CONF_OPT += 'EVOSN = "5076"'
+# Add IP Alias
+LOCAL_CONF_OPT += 'IP_ALIAS = "172.16.106.8/24"'
