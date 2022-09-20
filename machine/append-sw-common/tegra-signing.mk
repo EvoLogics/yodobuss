08a674b093f21d064bbc900e50dfbeec06ea72b0
llewellyn-evo <llewellyn.fernandes@evologics.de>
2022-09-20 11:07:31 +0200

    Update keys for kernel signing, ssd encryption

--- machine/append-sw-common/tegra-signing.mk
+++ machine/append-sw-common/tegra-signing.mk
@@ -1,5 +1,6 @@
 # Needed for SSD Encryption 
-LOCAL_CONF_OPT   += 'IMAGE_INSTALL_append = " cryptsetup keystore-tools keystore-tools-eksgen keystore"'
+LOCAL_CONF_OPT   += 'IMAGE_INSTALL_append = " cryptsetup evo-envinit-crypt"'
+# Bundle Initramfs
+LOCAL_CONF_OPT   += 'INITRAMFS_IMAGE_BUNDLE = "1"'
 # Encyption keys
-LOCAL_CONF_OPT   += 'TEGRA_SIGNING_ARGS = "-u /work/private/tegra194-evo-priv.pem -v /work/private/tegra194-evo-sbk"'
-
+LOCAL_CONF_OPT   += 'TEGRA_SIGNING_ARGS = "-u /work/private/tegra194-evo-priv.pem -v /work/private/tegra194-evo-sbk --user_key /work/private/tegra194-evo-uk "'
