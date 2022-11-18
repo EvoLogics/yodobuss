b34c2d5e0508338e8dc94ceb6b4fc3ff984bf120
llewellyn-evo <llewellyn.fernandes@evologics.de>
2022-11-18 12:37:18 +0100

    Add image signing for sonobot-50{67-68}

--- machine/tegra194-evo/sonobot-5067.mk
+++ machine/tegra194-evo/sonobot-5067.mk
@@ -1,10 +1,11 @@
 include machine/tegra194-evo/default.mk
 include machine/append-sw-common/object-recognition.mk
+include machine/append-sw-common/tegra-signing.mk
 
 LOCAL_CONF_OPT   += 'IMAGE_INSTALL_append = " db-agent"'
 # Enable all 6 cores by setting nvpmodule to mode 2
-LOCAL_CONF_OPT += 'NVPMODEL_CONFIG_DEFAULT = "2"'
+LOCAL_CONF_OPT   += 'NVPMODEL_CONFIG_DEFAULT = "2"'
 # Sonobot number
-LOCAL_CONF_OPT += 'EVOSN = "5067"'
+LOCAL_CONF_OPT   += 'EVOSN = "5067"'
 # Add IP Alias
-LOCAL_CONF_OPT += 'IP_ALIAS = "172.16.97.8/24"'
+LOCAL_CONF_OPT   += 'IP_ALIAS = "172.16.97.8/24"'
