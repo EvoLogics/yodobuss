c5946d8e1bd6c2bcd68269a9dce6f7c5007841c0
llewellyn-evo <llewellyn.fernandes@evologics.de>
2022-12-16 03:00:47 +0100

    Add swupdate-client to image

--- machine/append-sw-common/swupdate.mk
+++ machine/append-sw-common/swupdate.mk
@@ -3,5 +3,5 @@ include machine/append-sw-common/nodejs-native.mk
 LAYERS += git@gitlab.evologics.de:bsp/meta-evo-private.git
 
 # Install SWUPDATE
-LOCAL_CONF_OPT    += 'CORE_IMAGE_EXTRA_INSTALL += "swupdate swupdate-webapp-evo swupdate-hwrevision"'
+LOCAL_CONF_OPT    += 'CORE_IMAGE_EXTRA_INSTALL += "swupdate swupdate-webapp-evo swupdate-hwrevision swupdate-client "'
 LOCAL_CONF_OPT 	  += 'PREFERRED_VERSION_swupdate = "2021.04"'
