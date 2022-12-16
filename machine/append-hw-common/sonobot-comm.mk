0280af0624e0eb5f4d505980274b7de7d067069c
llewellyn-evo <llewellyn.fernandes@evologics.de>
2022-12-16 03:01:29 +0100

    Add dune-www to comm module

--- machine/append-hw-common/sonobot-comm.mk
+++ machine/append-hw-common/sonobot-comm.mk
@@ -2,7 +2,7 @@ include machine/append-sw-common/qemu-target.mk
 
 LAYERS += git@gitlab.evologics.de:bsp/meta-evo-private.git
 
-LOCAL_CONF_OPT += 'IMAGE_INSTALL_append = " dune-sonobot-comm-$${EVOSN} dune-sonobot-comm-$${EVOSN}-etc dune-sonobot-comm-$${EVOSN}-default"'
+LOCAL_CONF_OPT += 'IMAGE_INSTALL_append = " dune-sonobot-comm-$${EVOSN} dune-sonobot-comm-$${EVOSN}-etc dune-sonobot-comm-$${EVOSN}-default dune-sonobot-comm-$${EVOSN}-www"'
 
 LOCAL_CONF_OPT += 'hostname_pn-base-files = "sonobot-$${EVOSN}-comm"'
 
