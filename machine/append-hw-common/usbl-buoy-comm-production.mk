c1d8c9c987364044435bad73cd0acbe4062d9c1f
llewellyn-evo <llewellyn.fernandes@evologics.de>
2023-01-06 14:36:50 +0100

    Add dune-www to production usbl buoys

--- machine/append-hw-common/usbl-buoy-comm-production.mk
+++ machine/append-hw-common/usbl-buoy-comm-production.mk
@@ -6,6 +6,6 @@ LOCAL_CONF_OPT += 'INHERIT += " userconfig"'
 
 LOCAL_CONF_OPT += 'IMAGE_INSTALL_append = " htop soft-hwclock"'
 
-LOCAL_CONF_OPT += 'IMAGE_INSTALL_append = " dune-usbl-buoy dune-usbl-buoy-etc dune-usbl-buoy-default"'
+LOCAL_CONF_OPT += 'IMAGE_INSTALL_append = " dune-usbl-buoy dune-usbl-buoy-etc dune-usbl-buoy-default dune-usbl-buoy-www"'
 
 LOCAL_CONF_OPT += 'hostname_pn-base-files = "usbl-buoy-sn-$${EVOSN}"'
