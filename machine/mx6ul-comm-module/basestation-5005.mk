b15517fa8750dcc8da5a6d1bb797f019cb6abdc3
llewellyn-evo <llewellyn.fernandes@evologics.de>
2023-03-16 11:40:37 +0100

    Update config for basestation-5005

--- machine/mx6ul-comm-module/basestation-5005.mk
+++ machine/mx6ul-comm-module/basestation-5005.mk
@@ -1,8 +1,9 @@
 include machine/mx6ul-comm-module/default.mk
-include machine/append-hw-common/basestation-comm.mk
+include machine/append-hw-common/sonobot-basestation-comm.mk
+include machine/append-sw-common/swupdate.mk
 
 LOCAL_CONF_OPT += 'BRIDGE_ADDRESS="172.16.35.31/16"'
 
 LOCAL_CONF_OPT += 'BRIDGE_GATEWAY="172.16.35.30"'
 
-LOCAL_CONF_OPT += 'EVOSN = "5"'
+LOCAL_CONF_OPT += 'EVOSN = "5005"'
