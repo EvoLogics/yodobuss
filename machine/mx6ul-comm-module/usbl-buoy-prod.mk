9a1183bdad3f4f6dec20db22723bbda140a77a5b
llewellyn-evo <llewellyn.fernandes@evologics.de>
2023-03-16 11:41:05 +0100

    Add configuration for usbl-buoy-production image

---
+++ machine/mx6ul-comm-module/usbl-buoy-prod.mk
@@ -0,0 +1,15 @@
+include machine/mx6ul-comm-module/default.mk
+include machine/append-sw-common/swupdate.mk
+include machine/append-hw-common/usbl-buoy-comm-production.mk
+
+LOCAL_CONF_OPT += 'PREFERRED_VERSION_sinaps = "2.2.5"'
+
+LOCAL_CONF_OPT += 'HW_REVISION = "2.0"'
+
+LOCAL_CONF_OPT += 'EVOSN = "prod"'
+
+LOCAL_CONF_OPT += 'BRIDGE_ADDRESS="192.168.0.2/24"'
+
+LOCAL_CONF_OPT += 'BRIDGE_GATEWAY="192.168.0.1"'
+
+LOCAL_CONF_OPT += 'IMAGE_CONFIGS = " gpspps"'
