9f4ca6584cffd344a82e97efdd30e3292c0d62c3
Victor Voronin <viktor.voronin@evologics.de>
2023-10-04 17:06:20 +0200

    machine/append-sw-common/erlang.mk: change to hardknott

--- machine/append-sw-common/erlang.mk
+++ machine/append-sw-common/erlang.mk
@@ -1,6 +1,6 @@
 # Required layer
-LAYERS += https://github.com/meta-erlang/meta-erlang.git;branch=zeus
+LAYERS += https://github.com/meta-erlang/meta-erlang.git;branch=hardknott
 
 # Erlang OTP21
-LOCAL_CONF_OPT += 'PREFERRED_VERSION_erlang = "21.1%"'
-LOCAL_CONF_OPT += 'PREFERRED_VERSION_erlang-native = "21.1%"'
+LOCAL_CONF_OPT += 'PREFERRED_VERSION_erlang = "21.%"'
+LOCAL_CONF_OPT += 'PREFERRED_VERSION_erlang-native = "21.%"'
