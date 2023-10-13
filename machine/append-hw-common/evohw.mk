6d2eb9ea14d32f6746d4901b0db0b53faa103757
Victor Voronin <viktor.voronin@evologics.de>
2023-10-13 09:47:14 +0200

    machine/append-hw-common/evohw.mk: add dune-www

--- machine/append-hw-common/evohw.mk
+++ machine/append-hw-common/evohw.mk
@@ -1,5 +1,5 @@
 include machine/append-sw-common/dune.mk
 
 LOCAL_CONF_OPT += 'MACHINEOVERRIDES =. "$${EVOHW}:$${EVOHW}-$${EVOSN}:"' \
-                  'CORE_IMAGE_EXTRA_INSTALL += "dune-$${EVOHW}-$${EVOSN} dune-$${EVOHW}-$${EVOSN}-etc dune-$${EVOHW}-$${EVOSN}-default "' \
+                  'CORE_IMAGE_EXTRA_INSTALL += "dune-$${EVOHW}-$${EVOSN} dune-$${EVOHW}-$${EVOSN}-etc dune-$${EVOHW}-$${EVOSN}-default dune-$${EVOHW}-$${EVOSN}-www "' \
                   'hostname_pn-base-files = "$${EVOHW}-$${EVOSN}"'
