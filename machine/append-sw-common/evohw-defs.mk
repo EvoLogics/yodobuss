8edafbc01a25e630ca6f6eacdfe18e66877fd1da
Victor Voronin <viktor.voronin@evologics.de>
2023-10-25 14:30:31 +0200

    machine/append-sw-common: add evohw-defs.mk

---
+++ machine/append-sw-common/evohw-defs.mk
@@ -0,0 +1,5 @@
+# evotools come from meta-evo-private
+LAYERS += git@gitlab.evologics.de:bsp/meta-evo-private.git
+
+# Install necessary packages.
+LOCAL_CONF_OPT += 'EVO_BASE_EXTRA_INSTALL += "evohw-defs evo-assets"'
