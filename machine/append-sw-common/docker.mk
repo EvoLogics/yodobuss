209f1a2f33d22f5f5284537cac42e79597ee91fa
Victor Voronin <viktor.voronin@evologics.de>
2023-10-30 09:02:36 +0100

    machine/append-sw-common: add docker.mk

---
+++ machine/append-sw-common/docker.mk
@@ -0,0 +1,6 @@
+# Required layer
+LAYERS += git://git.yoctoproject.org/meta-virtualization
+
+LOCAL_CONF_OPT += 'DISTRO_FEATURES_append = " virtualization "'
+LOCAL_CONF_OPT += 'PREFERRED_PROVIDER_virtual/docker = "docker-moby"'
+LOCAL_CONF_OPT += 'EVO_BASE_EXTRA_INSTALL += "docker python3-docker-compose"'
