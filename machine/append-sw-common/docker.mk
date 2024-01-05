527451d8767c618a337890482586570e2b574815
Victor Voronin <viktor.voronin@evologics.de>
2024-01-05 15:37:55 +0100

    machine//docker.mk: explicitly add distutils

--- machine/append-sw-common/docker.mk
+++ machine/append-sw-common/docker.mk
@@ -3,4 +3,4 @@ LAYERS += git://git.yoctoproject.org/meta-virtualization
 
 LOCAL_CONF_OPT += 'DISTRO_FEATURES_append = " virtualization "'
 LOCAL_CONF_OPT += 'PREFERRED_PROVIDER_virtual/docker = "docker-moby"'
-LOCAL_CONF_OPT += 'EVO_BASE_EXTRA_INSTALL += "docker python3-docker-compose"'
+LOCAL_CONF_OPT += 'EVO_BASE_EXTRA_INSTALL += "docker python3-docker-compose python3-distutils"'
