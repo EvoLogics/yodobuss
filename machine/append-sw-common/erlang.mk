33127fc5598aafe6cd31af6535666585de0d9918
Victor Voronin <viktor.voronin@evologics.de>
2024-01-09 13:06:46 +0100

    machine/append-sw-common/erlang: use honister with patch

--- machine/append-sw-common/erlang.mk
+++ machine/append-sw-common/erlang.mk
@@ -1,5 +1,5 @@
 # Required layer
-LAYERS += https://github.com/meta-erlang/meta-erlang.git;branch=hardknott
+LAYERS += https://github.com/meta-erlang/meta-erlang.git;branch=honister;patches=0001-honister-fix-for-kirkstone.patch
 
 # Erlang OTP21
 LOCAL_CONF_OPT += 'PREFERRED_VERSION_erlang = "21.%"'
