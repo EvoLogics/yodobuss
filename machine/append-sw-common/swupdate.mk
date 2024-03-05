f04225cf447254f7db9298546fba7735afe782ab
Victor Voronin <viktor.voronin@evologics.de>
2024-03-05 10:04:41 +0100

    machine/append-sw-common/swupdate.mk: switch version

--- machine/append-sw-common/swupdate.mk
+++ machine/append-sw-common/swupdate.mk
@@ -4,4 +4,4 @@ LAYERS += git@gitlab.evologics.de:bsp/meta-evo-private.git
 
 # Install SWUPDATE
 LOCAL_CONF_OPT    += 'EVO_BASE_EXTRA_INSTALL += "swupdate swupdate-webapp-evo swupdate-hwrevision swupdate-client "'
-LOCAL_CONF_OPT 	  += 'PREFERRED_VERSION_swupdate = "2021.04"'
+LOCAL_CONF_OPT 	  += 'PREFERRED_VERSION_swupdate = "2023.05"'
