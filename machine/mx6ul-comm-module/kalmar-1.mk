d801bccd607045f07e96f4b8f173d5380e8876bc
llewellyn-evo <llewellyn.fernandes@evologics.de>
2024-04-02 07:53:36 +0200

    Add config for kalmar-1

---
+++ machine/mx6ul-comm-module/kalmar-1.mk
@@ -0,0 +1,13 @@
+include machine/mx6ul-comm-module/default.mk
+include machine/append-sw-common/swupdate.mk
+include machine/append-hw-common/quadroin.mk
+
+LOCAL_CONF_OPT += 'EVOSN = "1"'
+
+LOCAL_CONF_OPT += 'HW_REVISION = "2.0"'
+
+LOCAL_CONF_OPT += 'IMAGE_CONFIGS = " can enablesw atmclkpps"'
+
+LOCAL_CONF_OPT += 'hostname_pn-base-files = "kalmar-$${EVOSN}"'
+
+LOCAL_CONF_OPT += 'INHERIT += " userconfig"'
