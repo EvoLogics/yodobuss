e45f14f7702d6949121ca3beede98e3925b585fc
llewellyn-evo <llewellyn.fernandes@evologics.de>
2024-04-09 15:03:19 +0200

    Added common config for sonobot-tegra

---
+++ machine/append-hw-common/sonobot-tegra.mk
@@ -0,0 +1,5 @@
+include machine/append-sw-common/qemu-target.mk
+
+LAYERS += git@gitlab.evologics.de:bsp/meta-evo-private.git
+
+LOCAL_CONF_OPT += 'IMAGE_INSTALL_append = " dune-sonobot-tegra-$${EVOSN} dune-sonobot-tegra-$${EVOSN}-etc dune-sonobot-tegra-$${EVOSN}-default"'
