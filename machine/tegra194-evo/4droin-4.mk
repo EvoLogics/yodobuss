d90059db57882abff9b8eb1e6cf1e639649bb9b4
llewellyn-evo <llewellyn.fernandes@evologics.de>
2024-07-18 22:40:05 +0200

    update configs for tegra 4droin-1,2,3,4

--- machine/tegra194-evo/4droin-4.mk
+++ machine/tegra194-evo/4droin-4.mk
@@ -1,14 +1,13 @@
 include machine/tegra194-evo/default.mk
 #include machine/append-sw-common/tegra-signing.mk
-#include machine/append-sw-common/collision-avoidance.mk
 
 # Enable all 6 cores by setting nvpmodule to mode 2
 LOCAL_CONF_OPT += 'NVPMODEL_CONFIG_DEFAULT = "2"'
 # Hostname
-LOCAL_CONF_OPT    += 'hostname_pn-base-files = "4Droin-1-tegra"'
+LOCAL_CONF_OPT    += 'hostname_pn-base-files = "4Droin-4-tegra"'
 # v4l
 LOCAL_CONF_OPT   += 'IMAGE_INSTALL_append = " v4l-utils libv4l libv4l-dev "'
 #Debug
 LOCAL_CONF_OPT   += 'IMAGE_INSTALL_append = " gdb packagegroup-core-buildessential cmake python3-smbus"'
 #OpenCV
-LOCAL_CONF_OPT   += 'IMAGE_INSTALL_append = " packagegroup-gstreamer dune-4droin-tegra dune-4droin-tegra-etc dune-4droin-tegra-default"'
+LOCAL_CONF_OPT   += 'IMAGE_INSTALL_append = " packagegroup-gstreamer dune-4droin-tegra"'
