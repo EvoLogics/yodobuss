6ad0c7ed1bb2e871da2a7b0e88742e0d02c6bc1e
llewellyn-evo <llewellyn.fernandes@evologics.de>
2024-07-18 22:55:53 +0200

    Add config for sonobot-5{017-09, 167-190}

---
+++ machine/mx6ul-comm-module/sonobot-5018.mk
@@ -0,0 +1,14 @@
+include machine/mx6ul-comm-module/default.mk
+include machine/append-hw-common/sonobot-comm.mk
+include machine/append-hw-common/sonobot-comm-streamcaster.mk
+include machine/append-sw-common/swupdate.mk
+
+LOCAL_CONF_OPT += 'HW_REVISION = "2.0"'
+
+LOCAL_CONF_OPT += 'EVOSN = "5018"'
+
+LOCAL_CONF_OPT += 'IMAGE_CONFIGS += " gpspps "'
+
+LOCAL_CONF_OPT += 'EXTERNAL_IP="172.16.48.2/16"'
+
+LOCAL_CONF_OPT += 'EXTERNAL_GATEWAY="172.16.48.2"'
