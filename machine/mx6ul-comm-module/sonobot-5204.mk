03f67fe3e3ee3564b2c42c70826d13efb7cf930e
Llewellyn Fernandes <llewellyn.fernandes@evologics.de>
2024-08-19 22:07:24 +0200

    Add configutaion for sonobot-5{194-205}

---
+++ machine/mx6ul-comm-module/sonobot-5204.mk
@@ -0,0 +1,12 @@
+include machine/mx6ul-comm-module/default.mk
+include machine/append-hw-common/sonobot-comm.mk
+include machine/append-hw-common/sonobot-comm-streamcaster.mk
+include machine/append-sw-common/swupdate.mk
+
+LOCAL_CONF_OPT += 'HW_REVISION = "2.0"'
+
+LOCAL_CONF_OPT += 'EVOSN = "5204"'
+
+LOCAL_CONF_OPT += 'IMAGE_CONFIGS += " gpspps "'
+
+LOCAL_CONF_OPT += 'EXTERNAL_IP="172.16.234.2/16"'
