fd07f10d15c73e9e3e31d5c2948b4744b7ecb50a
Llewellyn Fernandes <llewellyn.fernandes@evologics.de>
2024-08-31 07:53:23 +0200

    Update recipe for sonobot-50{02,77}

--- machine/mx6ul-comm-module/sonobot-5077.mk
+++ machine/mx6ul-comm-module/sonobot-5077.mk
@@ -1,13 +1,17 @@
 include machine/mx6ul-comm-module/default.mk
 include machine/append-hw-common/sonobot-comm.mk
-include machine/append-hw-common/sonobot-comm-streamcaster.mk
-include machine/append-hw-common/sonobot-comm-iridium.mk
+#include machine/append-hw-common/sonobot-comm-streamcaster.mk
+#include machine/append-hw-common/sonobot-comm-iridium.mk
 include machine/append-sw-common/swupdate.mk
+
+LAYERS += git@gitlab.evologics.de:bsp/meta-evo-private.git
 
 LOCAL_CONF_OPT += 'HW_REVISION = "2.0"'
 
 LOCAL_CONF_OPT += 'EVOSN = "5077"'
 
-LOCAL_CONF_OPT += 'IMAGE_CONFIGS += " gpspps "'
+LOCAL_CONF_OPT += 'IMAGE_INSTALL_append = " smcroute evo-firewall networkd-dispatcher"'
+
+LOCAL_CONF_OPT += 'IMAGE_CONFIGS = " gpspps can enablesw atmclkpps streamcaster"'
 
 LOCAL_CONF_OPT += 'EXTERNAL_IP="172.16.107.2/16"'
