c7ad7c668eced431bd24be8abf9f84681511c951
Llewellyn Fernandes <llewellyn.fernandes@evologics.de>
2024-08-31 07:47:22 +0200

    Add recipe for sonobot/basestation-50{25,26}

---
+++ machine/mx6ul-comm-module/sonobot-5025.mk
@@ -0,0 +1,15 @@
+include machine/mx6ul-comm-module/default.mk
+include machine/append-hw-common/sonobot-comm.mk
+include machine/append-sw-common/swupdate.mk
+
+LAYERS += git@gitlab.evologics.de:bsp/meta-evo-private.git
+
+LOCAL_CONF_OPT += 'HW_REVISION = "2.0"'
+
+LOCAL_CONF_OPT += 'EVOSN = "5025"'
+
+LOCAL_CONF_OPT += 'IMAGE_INSTALL_append = " smcroute evo-firewall networkd-dispatcher"'
+
+LOCAL_CONF_OPT += 'IMAGE_CONFIGS = " gpspps can enablesw atmclkpps streamcaster"'
+
+LOCAL_CONF_OPT += 'EXTERNAL_IP="172.16.55.2/16"'
