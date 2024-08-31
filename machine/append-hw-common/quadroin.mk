c8fe6f4590b26096fbe84c8d1098d94341c4f50f
Llewellyn Fernandes <llewellyn.fernandes@evologics.de>
2024-08-31 07:52:12 +0200

    Add dune-www to quadroin

--- machine/append-hw-common/quadroin.mk
+++ machine/append-hw-common/quadroin.mk
@@ -2,7 +2,7 @@ include machine/append-sw-common/qemu-target.mk
 
 LAYERS += git@gitlab.evologics.de:bsp/meta-evo-private.git
 
-LOCAL_CONF_OPT += 'IMAGE_INSTALL_append = " dune-quadroin-$${EVOSN} dune-quadroin-$${EVOSN}-etc dune-quadroin-$${EVOSN}-default "'
+LOCAL_CONF_OPT += 'IMAGE_INSTALL_append = " dune-quadroin-$${EVOSN} dune-quadroin-$${EVOSN}-etc dune-quadroin-$${EVOSN}-default dune-quadroin-$${EVOSN}-www"'
 
 LOCAL_CONF_OPT += 'IMAGE_INSTALL_append = " libevocanopen xsenstool cannelloni"'
 
