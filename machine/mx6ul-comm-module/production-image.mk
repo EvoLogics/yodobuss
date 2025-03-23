2132cd0c03d1a2caa0a89ba30a7033cb466d0b9c
LLewellyn Fernandes <llewellyn.fernandes@evologics.com>
2025-03-23 07:51:44 +0100

    Update production images

--- machine/mx6ul-comm-module/production-image.mk
+++ machine/mx6ul-comm-module/production-image.mk
@@ -1,11 +1,26 @@
 include machine/mx6ul-comm-module/default.mk
 include machine/append-sw-common/swupdate.mk
 include machine/append-sw-common/qemu-target.mk
+include machine/append-sw-common/sinaps.mk
 
 LOCAL_CONF_OPT += 'hostname_pn-base-files = "production-comm-image"'
 
-LOCAL_CONF_OPT += 'IMAGE_CONFIGS = " can enablesw"'
+LOCAL_CONF_OPT += 'IMAGE_CONFIGS = " can enablesw gpspps EC25"'
 
 LOCAL_CONF_OPT += 'IMAGE_INSTALL_append = " dune-production-comm dune-production-comm-etc dune-production-comm-default comm-config "'
 
+LOCAL_CONF_OPT += 'INHERIT += " userconfig"'
+
 LOCAL_CONF_OPT += 'HW_REVISION = "2.0"'
+# Install for Zerotier VPN
+LOCAL_CONF_OPT += 'IMAGE_INSTALL_append = " zerotier"'
+# Install for LTE connection
+LOCAL_CONF_OPT += 'IMAGE_INSTALL_append = " libqmi quectel-cm busybox-udhcpc evo-nat evo-firewall smcroute "'
+
+LOCAL_CONF_OPT += 'IMAGE_INSTALL_append = " mosquitto-clients"'
+
+LOCAL_CONF_OPT += 'IMAGE_INSTALL_append = " htop soft-hwclock haproxy"'
+
+LOCAL_CONF_OPT += 'IMAGE_INSTALL_append = " mosquitto-clients"'
+
+#LOCAL_CONF_OPT += 'PREFERRED_VERSION_sinaps = "2.2.6"'
--- machine/mx6ul-comm-module/usbl-buoy-prod.mk
+++ machine/mx6ul-comm-module/usbl-buoy-prod.mk
@@ -12,4 +12,18 @@ LOCAL_CONF_OPT += 'BRIDGE_ADDRESS="192.168.0.2/24"'
 
 LOCAL_CONF_OPT += 'BRIDGE_GATEWAY="192.168.0.1"'
 
-LOCAL_CONF_OPT += 'IMAGE_CONFIGS = " gpspps"'
+LOCAL_CONF_OPT += 'IMAGE_CONFIGS = " gpspps EC25"'
+
+# Install for Zerotier VPN
+LOCAL_CONF_OPT += 'IMAGE_INSTALL_append = " zerotier"'
+# Install for LTE connection
+LOCAL_CONF_OPT += 'IMAGE_INSTALL_append = " libqmi quectel-cm busybox-udhcpc evo-nat evo-firewall smcroute iptables-persistent "'
+
+LOCAL_CONF_OPT += 'IMAGE_INSTALL_append = " mosquitto-clients"'
+
+#LOCAL_CONF_OPT += 'RDEPENDS_append = " openssh"'
+
+LOCAL_CONF_OPT += 'PREFERRED_VERSION_openssh = "9.9p1"'
+
+LOCAL_CONF_OPT += 'PACKAGECONFIG_pn-openssh_append = "systemd-sshd-socket-mode"'
+
